48a8c58637aac62004175d51e252354a80c4d885
Nahuel Paolinelli <nahuel.paolinelli@gmail.com>
2022-01-30 21:19:20 +0100

    Proyecto limpio y carpetas creadas

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
