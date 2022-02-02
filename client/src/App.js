3ee46f15a15f03e3b8248124a812582ae0512daa
Joaquin <71033509+joacogirardi@users.noreply.github.com>
2022-02-01 23:13:50 -0300

    Login component, and some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <Routes>
     <Route path='/' element={<LandingPage/>}/>
+    <Route exact path='/home/login' element={<Login />}/>
     </Routes>
     </div>
     </BrowserRouter>
