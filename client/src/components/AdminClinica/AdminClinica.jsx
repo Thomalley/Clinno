c2f82db307e11c4a23d061138925154e707c7882
PabloKazalukian <kazalukianpablo@gmail.com>
2022-02-09 11:50:10 -0300

    admin clinica

--- client/src/components/AdminClinica/AdminClinica.jsx
+++ client/src/components/AdminClinica/AdminClinica.jsx
@@ -4,44 +4,55 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import Footer from "../Home/Footer";
 import swal from 'sweetalert';
+import NavClinica from './NavClinica.jsx'
+import {get_clinica} from '../../actions';
 
 
 import Cookies from 'universal-cookie';
+import "./AdminClinicaStyle.css";
 
 
 export default function LoginClinica(){
-    
-    const cookies = new Cookies()
+
+    const dispatch = useDispatch();
+
+    const cookies = new Cookies();
+    const clinica = useSelector((state)=> state.clinica[0]);
+
+    useEffect(() => {
+        dispatch(get_clinica(cookies.get('clinica_id')));
+    },[])
 
     let session=false;
-    // console.log("sesion iniciada por " + cookies.get('email'))  
-    if(cookies.get('clinica_mail')){
+
+    //control se dession
+    useEffect(() => {
+        console.log('workin')
+        if(cookies.get('clinica_id')) {
+            setLoggeado(true)
+        }else{
+            setLoggeado(false)
+        }
+    },[cookies.get('clinica_id')])
+
+    if(cookies.get('clinica_id')){
         session = true;
     }
     const [loggeado,setLoggeado] = useState(session);
 
-    const cerrarSesion=()=>{
-
-        cookies.remove('clinica_mail');
-        cookies.remove('clinica_nombre');
-        cookies.remove('clinica_telefono');
-        cookies.remove('clinica_direccion');
-        cookies.remove('clinica_id');
-        cookies.remove('clinica_nombreEn', );
-        cookies.remove('clinica_apellidoEn');
-        cookies.remove('clinica_DNIEn');
-        cookies.remove('clinica_createdAt');
-
-        setLoggeado(false);
-        swal("Has cerrado la sesion con explito!!", "En instantes seras redirigido a Inicio", "success")
-
-    }
+    
 
     if(loggeado){
         return(
-            <div>
-                <h1>Hola Mundo</h1>
-                <button className="btn btn-outline border-start " type="button" onClick={cerrarSesion}>Cerrar sesion</button>
+            <div >
+                <div className="contenedor_adminClinica">
+                    <NavClinica/>
+                    <h1>Bienvenido {clinica?.nombre}</h1>
+                    <h1>Welcome to cookis {cookies.get('clinica_nombre')}</h1>                 
+                    
+
+
+                </div>
 
                 <Footer />
     
