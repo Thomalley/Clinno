ba6b7c6a02b4010a94ef38d439db0e3c87eb6513
Joaquin <71033509+joacogirardi@users.noreply.github.com>
2022-02-14 11:30:14 -0300

    Bug fix

--- client/src/components/EstamosTrabajando/EstamosTrabajando.jsx
+++ client/src/components/EstamosTrabajando/EstamosTrabajando.jsx
@@ -1,18 +1,27 @@
 import React from "react"
 import './EstamosTrabajando.css'
 import photo from '../utils/hipertencion.gif'
-export default function EstamosTrabajando() {
+import { Link } from "react-router-dom";
 
+
+export default function EstamosTrabajando() {
 
     return (
         <div className="contiene">
             <div className="container">
                 <div className=".no-gutters">
                     <div className="col-12">
-                        <h1 id="work" className="display-5">Estamos trabajando para usted!</h1>
-                        <img className="workingImage" src={photo} alt="estamos trabando" />
+                        <h1 id="work" className="display-5">Estamos trabajando <strong>para usted!</strong></h1>
+                        <img  className=" col-10 .col-sm-8" id="workingImage" src={photo} alt="estamos trabando" />
                         <h2 id="build" className="display-6">Componente en <strong>construcción</strong></h2>
+                        <Link to='/'>
+                            <button className="btn btn-warning">
+                                Volver a inicio
+                            </button>
+                        </Link>
                     </div>
+                </div>
+                <div className="foot_er">
                 </div>
             </div>
         </div>
